b24f46c518c549855551f7a4ead10dce0793df97
Greg Hale <imalsogreg@gmail.com>
2020-03-07 21:52:43 -0500

    Bump dependencies for ghc-8.8.2

--- release.nix
+++ release.nix
@@ -1,6 +1,8 @@
-{ compiler ? "default" }:
+{ compiler ? "default"
+, nixpkgs  ? null
+}:
 let
-  pkgs = import ./pkgs.nix;
+  pkgs = import ./pkgs.nix { inherit nixpkgs; };
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
